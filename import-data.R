e4d6056cf86a6f3fbdf8c771e954ddb851462b38
erkind <erkindoglu@gmail.com>
2024-03-10 21:06:26 +0100

    Update import-data.R

--- import-data.R
+++ import-data.R
@@ -3,4 +3,4 @@ data1 <- read.csv(paste("http://raw.githubusercontent.com/",
                         "erkind/dissertation-hm/",
                         "main/data-cut.csv",
                         sep = ""),
-                  header = TRUE, sep = "")
+                  header = TRUE, sep = ",")
