f144b7ada6760aeff62028416ffe92740bb4ae4a
erkind <erkindoglu@gmail.com>
2024-03-10 21:17:46 +0100

    Update import-data.R

--- import-data.R
+++ import-data.R
@@ -4,3 +4,13 @@ data1 <- read.csv(paste("http://raw.githubusercontent.com/",
                         "main/data-cut.csv",
                         sep = ""),
                   header = TRUE, sep = ",")
+colnames(data1)
+data2 <- read.csv(paste("http://raw.githubusercontent.com/",
+                        "erkind/dissertation-hm/",
+                        "main/data-hedge-funds.csv",
+                        sep = ""),
+                  header = TRUE, sep = ",")
+dim(data1)
+dim(data2)
+colnames(data1)
+colnames(data2)
