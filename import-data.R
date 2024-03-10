fd8424f6bb3c2f5e1967ff226682939d33a5fa28
erkind <erkindoglu@gmail.com>
2024-03-10 21:00:45 +0100

    Create import-data.R

---
+++ import-data.R
@@ -0,0 +1,6 @@
+# load original csv files
+data1 <- read.csv(paste("http://raw.githubusercontent.com/",
+                        "erkind/dissertation-hm/",
+                        "main/data-cut.csv",
+                        sep = ""),
+                  header = TRUE, sep = "")
